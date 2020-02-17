9f56af6d76f53b54b37d0298b65b6671a02a25e3
kiryil <kiryil12@gmail.com>
2020-02-17 19:40:12 +0300

    test

---
+++ src/components/Profile.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+
+const Profile = () =>{
+    return <div className="content">
+    <div>
+      <img src="https://upload.wikimedia.org/wikipedia/commons/f/f7/Yosemite-stream.jpg" />
+    </div>
+    <div>
+      ava + descr
+    </div>
+    <div>
+      My posts
+      <div>
+        New post
+      </div>
+      <div>
+        <div>
+          post 1 
+        </div>
+      </div>
+    </div>
+  </div>
+}
+
+export default Profile;
